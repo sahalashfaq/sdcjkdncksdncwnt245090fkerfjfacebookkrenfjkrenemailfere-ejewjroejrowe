d0b71bc19916ed535e9f2de0831098f14c085da0
sahalashfaq <145576065+sahalashfaq@users.noreply.github.com>
2025-08-02 22:30:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,23 +20,20 @@ local_css("style.css")
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 def init_driver():
     chrome_options = Options()
-    chrome_options.binary_location = "/usr/bin/chromium"  # ✅ Correct path on Streamlit Cloud
+    chrome_options.binary_location = "/usr/bin/chromium"
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920x1080")
 
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
+        service=Service("/usr/bin/chromedriver"),  # ✅ Manually installed correct driver
         options=chrome_options
     )
     return driver
-
 
 
 def scrape_emails_from_url(url):
@@ -113,5 +110,6 @@ if uploaded_file:
             "Scraped_by_the_SeekGps.csv",
             "text/csv"
         )
+
 
 
