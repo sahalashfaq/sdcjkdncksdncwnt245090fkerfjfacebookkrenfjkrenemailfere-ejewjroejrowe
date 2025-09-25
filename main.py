ba2b62b1f3f43ae1864f6bd03b3fb534ca8ba00d
sahalashfaq <145576065+sahalashfaq@users.noreply.github.com>
2025-09-25 13:57:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from concurrent.futures import ThreadPoolExecutor
 
 # ----------------- Custom CSS Loader --------------------
 def local_css(file_name):
-    with open(file_name) as f:
-st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 local_css("style.css")
 
 # ----------------- Scraper Logic --------------------
@@ -169,5 +170,6 @@ csv_data,
 )
 
 asyncio.run(scrape_and_display())
+
 
 
